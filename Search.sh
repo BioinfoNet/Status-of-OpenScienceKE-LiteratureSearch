7f11fbace490eb1ae6b267e0122ee061eb2a8f53
esohkevin <esohkevin4@gmail.com>
2018-08-29 13:05:14 +0300

    minor edit

--- Search.sh
+++ Search.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-
-# Searching the PubMed and PMC databases for Open Science papers with Kenyan Affiliations
-# esearch -db pubmed -query "open science[keyword] OR \
-#	open source[keyword] OR \
-#	open data[keyword] OR \
-#	open access[keyword] OR \
-#	open peer review[keyword] OR \
-#	open review[keyword] OR \
-#	open notebooks[keyword] OR \
-#	open educational resources[keyword] OR \
-#	open resource[keyword] OR \
-#	citizen science[keyword] OR \
-#	scientific social network[keyword]" |
-# efetch -format xml |	# Retrieve the xml format of all the articles with Open Science keywords
-
 ### Running for loops with Open Science keywords against the PubMed database ###
 # A count of the PubMed search results for the corresponding keywords
 for kwd in \
@@ -34,7 +19,6 @@ do
 	efetch -format uid | 
 	wc -l > keyCounts.txt
 done
-
 # Extract PMIDS, Journal titles, and Author LastNames of each paper, and their respective affiliations into tab separated columns
 for kwd in \
 	"open science" \
@@ -55,13 +39,11 @@ do
 	xtract -pattern MedlineCitation -sep "\t" -element MedlineCitation/PMID, Journal/Title -block Author -sep "\t" -element LastName, Affiliation |
 	grep "Kenya" | 
 	awk '{print $1}' > pmids_all.txt
-	
 	xtract -pattern PubmedArticle -sep "\t" -element MedlineCitation/PMID, Journal/Title, -first Author Affiliation |
 	grep "Kenya"
 	awk '{print $1}' > pmids_fa.txt
 done
 echo -e "\nSearch completed!"
-
 # Articles in the Open Access (OA) subset of PubMed Central (PMC)
 # esearch -db pmc -query "open access[filter]" | 
 # efetch -format xml > pmcOA.txt
